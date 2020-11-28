210f55fa080cf7a08ad23f14cf8762e87894d5e9
secret_survivor <c.mcbrayne@yahoo.co.uk>
2020-11-28 23:33:07 +0000

    Update permission_module.lua
    Redunt code

--- lua/jaas/permission_module.lua
+++ lua/jaas/permission_module.lua
@@ -2,26 +2,17 @@ local permission = JAAS.Permission()
 
 local noclip = permission.registerPermission("Noclip")
 hook.Add("PlayerNoClip", "JAAS_noclipPermission", function (ply, desiredNoClipState)
-    if noclip:codeCheck(ply:getJAASCode()) or !desiredNoClipState then
-        return true
-    end
-    return false
+    return noclip:codeCheck(ply:getJAASCode()) or !desiredNoClipState
 end)
 
 local pickup = permission.registerPermission("Pickup")
 hook.Add("AllowPlayerPickup", "JAAS_pickupPermission", function (ply)
-    if pickup:codeCheck(ply:getJAASCode()) then
-        return true
-    end
-    return false
+    return pickup:codeCheck(ply:getJAASCode())
 end)
 
 local editVariables = permission.registerPermission("Can Edit Variables")
 hook.Add("CanEditVariable", "JAAS_canEditVariablesPermission", function (ent, ply, key, val, editor)
-    if editVariables:codeCheck(ply:getJAASCode()) then
-        return true
-    end
-    return false
+    return editVariables:codeCheck(ply:getJAASCode())
 end)
 
 local physgunPickupAllow = permission.registerPermission("Physgun Player Pickup Allow")
@@ -41,43 +32,31 @@ end)
 
 local canPlayerTaunt = permission.registerPermission("Can Player Taunt")
 hook.Add("PlayerShouldTaunt", "JAAS_canPlayerTauntPermission", function (ply, act)
-    if canPlayerTaunt:codeCheck(ply:getJAASCode()) then
-        return true
-    end
-    return false
+    return canPlayerTaunt:codeCheck(ply:getJAASCode())
 end)
 
 local canPlayerSpray = permission.registerPermission("Can Player Spray")
 hook.Add("PlayerSpray", "JAAS_canPlayerSprayPermission", function (ply)
-    if canPlayerSpray:codeCheck(ply:getJAASCode()) then
-        return true
-    end
-    return false
+    return canPlayerSpray:codeCheck(ply:getJAASCode())
 end)
 
 local canPickupItem = permission.registerPermission("Can Pickup Item")
 hook.Add("PlayerCanPickupItem", "JAAS_canPickupItemPermission", function (ply, item)
-    if canPickupItem:codeCheck(ply:getJAASCode()) then
-        return true
-    end
-    return false
+    return canPickupItem:codeCheck(ply:getJAASCode())
 end)
 
 local canPickupWeapon = permission.registerPermission("Can Pickup Weapon")
 hook.Add("PlayerCanPickupWeapon", "JAAS_canPickupWeaponPermission", function (ply, wep)
-    if canPickupWeapon:codeCheck(ply:getJAASCode()) then
-        return true
-    end
-    return false
+    return canPickupWeapon:codeCheck(ply:getJAASCode())
 end)
 
 local meta = FindMetaTable("Player")
 local isAdmin = permission.registerPermission("Is Admin")
 function meta:IsAdmin()
-    return isAdmin:codeCheck(self:getJAASCode()) and true
+    return isAdmin:codeCheck(self:getJAASCode())
 end
 
 local isSuperadmin = permission.registerPermission("Is Superadmin")
 function meta:IsSuperAdmin()
-    return isSuperadmin:codeCheck(self:getJAASCode()) and true
+    return isSuperadmin:codeCheck(self:getJAASCode())
 end
