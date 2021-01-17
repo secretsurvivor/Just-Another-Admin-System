c7e13f0571da179d38facac57c2bd798cfa3275f
secret_survivor <c.mcbrayne@yahoo.co.uk>
2021-01-17 21:04:11 +0000

    More Permissions
    Added a few more permissions to create more depth in a couple hooks.

--- lua/jaas/permission_module.lua
+++ lua/jaas/permission_module.lua
@@ -6,30 +6,39 @@ hook.Add("PlayerNoClip", "JAAS_noclipPermission", function (ply, desiredNoClipSt
 end)
 
 local pickup = permission.registerPermission("Pickup", "Player will be able to pick up objects with +USE")
-hook.Add("AllowPlayerPickup", "JAAS_pickupPermission", function (ply)
+hook.Add("AllowPlayerPickup", "JAAS_pickupPermission", function (ply, ent)
+    if ent:IsPlayer() then
+        return false
+    end
     return pickup:codeCheck(ply:getJAASCode())
 end)
 
 local editVariables = permission.registerPermission("Can Edit Variables", "Player will be able to edit entity variables")
-hook.Add("CanEditVariable", "JAAS_canEditVariablesPermission", function (ent, ply, key, val, editor)
+hook.Add("CanEditVariable", "JAAS_canEditVariablesPermission", function (_, ply)
     return editVariables:codeCheck(ply:getJAASCode())
 end)
 
 local physgunPickupAllow = permission.registerPermission("Physgun Player Pickup Allow", "Player will be able to pickup other players that they are able to target")
+local physgunMapEntityPickupAllow = permission.registerPermission("Physgun Map Entity Pickup Allow", "Player will be able to pickup map created entities")
+local globalPhysgunPickupAllow = permission.registerPermission("Physgun Pickup Allow", "Player will be able to pickup entities with Physgun")
 hook.Add("PhysgunPickup", "JAAS_physgunPickupAllowPermission", function (ply, ent)
     if ent:IsPlayer() then
-        if physgunPickupAllow:codeCheck(ply:getJAASCode()) and ply:canTarget(ent:getJAASCode()) then
-            return true
+        if ent:IsBot() then
+            return physgunPickupAllow:codeCheck(ply:getJAASCode())
+        else
+            return physgunPickupAllow:codeCheck(ply:getJAASCode()) and ply:canTarget(ent:getJAASCode())
         end
-    elseif ent:IsBot() then
-        if physgunPickupAllow:codeCheck(ply:getJAASCode()) then
-            return true
+    else
+        if ent:CreatedByMap() then
+            return physgunMapEntityPickupAllow:codeCheck(ply:getJAASCode())
+        else
+            return globalPhysgunPickupAllow:codeCheck(ply:getJAASCode())
         end
     end
 end)
 
 local canPlayerTaunt = permission.registerPermission("Can Player Taunt", "Player will be able to taunt")
-hook.Add("PlayerShouldTaunt", "JAAS_canPlayerTauntPermission", function (ply, act)
+hook.Add("PlayerShouldTaunt", "JAAS_canPlayerTauntPermission", function (ply)
     return canPlayerTaunt:codeCheck(ply:getJAASCode())
 end)
 
@@ -39,12 +48,12 @@ hook.Add("PlayerSpray", "JAAS_canPlayerSprayPermission", function (ply)
 end)
 
 local canPickupItem = permission.registerPermission("Can Pickup Item", "Player will be able to pickup items")
-hook.Add("PlayerCanPickupItem", "JAAS_canPickupItemPermission", function (ply, item)
+hook.Add("PlayerCanPickupItem", "JAAS_canPickupItemPermission", function (ply)
     return canPickupItem:codeCheck(ply:getJAASCode())
 end)
 
 local canPickupWeapon = permission.registerPermission("Can Pickup Weapon", "Player will be able to pickup weapons")
-hook.Add("PlayerCanPickupWeapon", "JAAS_canPickupWeaponPermission", function (ply, wep)
+hook.Add("PlayerCanPickupWeapon", "JAAS_canPickupWeaponPermission", function (ply)
     return canPickupWeapon:codeCheck(ply:getJAASCode())
 end)
 
@@ -58,12 +67,12 @@ hook.Add("PlayerCanSeePlayersChat", "JAAS_canSeeAllChatMessages", function (text
 end)
 
 local ignoreDamage = permission.registerPermission("Ignore Damage", "Player won't be able to take damage")
-hook.Add("PlayerShouldTakeDamage", "JAAS_allowDamage", function (ply, attacker)
+hook.Add("PlayerShouldTakeDamage", "JAAS_allowDamage", function (ply)
     return !ignoreDamage:codeCheck(ply:getJAASCode())
 end)
 
 local ignoreFallDamage = permission.registerPermission("Ignore Fall Damage", "Player will ignore all fall damage")
-hook.Add("GetFallDamage", "JAAS_ignoreFallDamage", function (ply, speed)
+hook.Add("GetFallDamage", "JAAS_ignoreFallDamage", function (ply)
     if ignoreFallDamage:codeCheck(ply:getJAASCode()) then
         return 0
     end
@@ -75,13 +84,31 @@ hook.Add("CanPlayerSuicide", "JAAS_canSuicide", function (ply)
 end)
 
 local gravGunPickup = permission.registerPermission("Gravity Gun Pickup", "Player will be able to pick up entities with the Gravity Gun")
+local gravGunPlayerPickup = permission.registerPermission("Gravity Gun Player Pickup", "Player will be able to pickup players with Gravity Gun")
 hook.Add("GravGunPickupAllowed", "JAAS_gravGunPickup", function (ply, ent)
-    return gravGunPickup:codeCheck(ply:getJAASCode())
+    if ent:IsPlayer() then
+        if ent:IsBot() then
+            return gravGunPlayerPickup:codeCheck(ply:getJAASCode())
+        else
+            return gravGunPlayerPickup:codeCheck(ply:getJAASCode()) and ply:canTarget(ent:getJAASCode())
+        end
+    else
+        if !ent:CreatedByMap() then
+            return gravGunPickup:codeCheck(ply:getJAASCode())
+        end
+    end
 end)
 
 local canGravGunPunt = permission.registerPermission("Can Gravity Gun Punt", "Player will be able to punt entities with the Gravity Gun (Primary Fire)")
 hook.Add("GravGunPunt", "JAAS_canGravGunPunt", function (ply, ent)
-    return canGravGunPunt:codeCheck(ply:getJAASCode())
+    if !ent:CreatedByMap() then
+        return canGravGunPunt:codeCheck(ply:getJAASCode())
+    end
+end)
+
+local canUndo = permission.registerPermission("Can Undo", "Player will be able to undo spawned entities")
+hook.Add("CanUndo", "JAAS_CanUndo", function (ply)
+    return canUndo:codeCheck(ply:getJAASCode())
 end)
 
 local meta = FindMetaTable("Player")
