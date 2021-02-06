c6b496412a3b3a2bc95e11748f352bedb6cdf6b6
secret_survivor <c.mcbrayne@yahoo.co.uk>
2021-02-06 09:04:48 +0000

    Update GUI Module
    Now it can be accessed and works properly

--- lua/jaas-core/JAAS-gui.lua
+++ lua/jaas-core/JAAS-gui.lua
@@ -1,13 +1,13 @@
 local MODULE, log, dev = JAAS:RegisterModule "GUI"
 
-local interface_tab, settings = {}, {} -- interface_tab = {{name, panel, order}*},  settings[name] = {{name, data_type, client_func, default value, description}*}
+local interface_tab, settings = {}, {} -- interface_tab = {name, panel, order}*,  settings[name] = {{name, data_type, client_func, default value, description}*}
 local current_interface = nil
 local open_func, close_func = function() end, function () end
 
 local jaas_interface = {
     BuildTabs = function (func)
-        for k,v in ipairs(interface_tab) do
-            func(v)
+        for k,v in pairs(interface_tab) do
+            func(k, v[1], v[2])
         end
     end,
     BuildSettings = function (func)
@@ -26,15 +26,21 @@ local jaas_interface = {
 local gui = {
     RegisterInterface = function (panel)
         current_interface = panel
+        panel:Hide()
         return setmetatable({panel = panel}, {__index = jaas_interface}), panel
     end,
     RegisterTab = function (name, panel, order)
-        interface_tab = {name, panel, order}
+        panel:Hide()
+        interface_tab[name] = {panel, order}
     end,
-    RegisterSettings = function (name, settings_info) settings_info
+    RegisterSettings = function (name, settings_info)
         settings[name] = settings_info
     end
 }
 
 concommand.Add("+open_jaas", function () open_func() end)
 concommand.Add("-open_jaas", function () close_func() end)
+
+MODULE.Access(MODULE.Class(gui, "jaas_gui_library"))
+
+log:print "Module Loaded"
