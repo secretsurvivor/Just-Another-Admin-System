5d1dce8d5ec5604b8f4dbd940eb9fa02a1e47767
secret_survivor <c.mcbrayne@yahoo.co.uk>
2022-03-03 22:42:22 +0000

    Net Code Checklist
    I figured out a way to organise Net Code and that is to make a checklist and Do blocks with comments connecting back to the checklist. Just writing the checklist showed that I was missing a crucial part to the Net Code and I thought that the module was finished so already the checklist was doing what it was meant to. This will repeated for all existing and future modules (Really wish I had done this sooner)

--- lua/jaas-core/JAAS_Player.lua
+++ lua/jaas-core/JAAS_Player.lua
@@ -60,7 +60,7 @@ function MODULE:SetPlayerCode(ply, code)
 		local old_value = player_table[ply:SteamID64()][1]
 		if ply:IsConnected() then
 			player_table[ply:SteamID64()][1] = code
-			Player_Hook_Run("OnModifiedCode")(ply, old_value)
+			Player_Hook_Run("OnModifiedCode")(ply, code, old_value)
 		end
 		return true
 	end
@@ -70,6 +70,28 @@ end
 function MODULE:XorPlayerCode(ply, code) -- Should be the primary method of modifying a player's code
 	return MODULE:SetPlayerCode(ply, bit.bxor(ply:GetCode(), code))
 end
+
+hook.Add("PlayerAuthed", J_NET:GetNetworkString(Update_OnConnect), function (ply)
+	local found_player = PlayerTable:Select(ply:SteamID64())
+
+	if found_player then
+		player_table[ply:SteamID64()] = {found_player.Code, found_player.LastConnected}
+	else
+		if PlayerTable:Insert(ply:SteamID64()) then
+			player_table[ply:SteamID64()] = {0, 0}
+		else
+			error("An error occurred whilst inserting new player")
+		end
+	end
+
+	Player_Hook_Run("OnConnect")(ply, player_table[ply:SteamID64()])
+end)
+
+hook.Add("PlayerDisconnected", J_NET:GetNetworkString(Update_OnDisconnect), function (ply)
+	PlayerTable:UpdateLastConnected(ply:SteamID64())
+	player_table[ply:SteamID64()][2] = os.time()
+	Player_Hook_Run("OnDisconnect", function () player_table[ply:SteamID64()] = nil end)(ply, player_table[ply:SteamID64()])
+end)
 
 local net_modification_message = {}
 
@@ -110,111 +132,168 @@ local function ModificationMessage(tab)
 end
 
 do -- Net Code
+	/*	Net Code Checklist
+		(X = Not Present, O = Present)
+		Server:
+			On Connect Sync (Send) : O
+			On Connect Update Clients (Send) : O
+			On Disconnect Remove from Table (Send) : 0
+
+			::On Update {1 = Code, 2 = LastConnected}::
+			On Code Update (Send) : O
+			On Last Connected Update (Send) : O
+
+			::Support Client Modification::
+			Code Modify (Receive) : O
+		Client:
+			On Connect Sync (Receive) : O
+			On Connect Update Clients (Receive) : O
+				Hook : O
+			On Disconnect Remove from Table (Receive) :
+				Hook : O
+
+			::On Update {1 = Code, 2 = LastConnected}::
+			On Code Update (Receive) : O
+				Hook : O
+			On Last Connected Update (Receive) : O
+
+			::Support Client Modification::
+			Code Modify (Send) : O
+	*/
 	local Player_Net_Sync = MODULE:RegisterNetworkType("Sync")
 	local Sync_OnConnect = Player_Net_Sync("OnConnect")
 
 	local Player_Net_Update = MODULE:RegisterNetworkType("Update")
 	local Update_OnConnect = Player_Net_Update("OnConnect")
 	local Update_OnDisconnect = Player_Net_Update("OnDisconnect")
+	local Update_PlayerCode = Player_Net_Update("PlayerCode")
 	local Update_Modify = Player_Net_Update("Modify")
 
 	if SERVER then
-		hook.Add("PlayerAuthed", J_NET:GetNetworkString(Update_OnConnect), function (ply)
-			local found_player = PlayerTable:Select(ply:SteamID64())
+		local function writePlayerInfo(ply, code, last_connected) -- Player NetWrite Function
+			net.WriteString(ply:UserID())
+			net.WriteUInt(code, 32)
+			net.WriteUInt(last_connected, 32)
+		end
 
-			if found_player then
-				player_table[ply:SteamID64()] = {found_player.Code, found_player.LastConnected}
-			else
-				if PlayerTable:Insert(ply:SteamID64()) then
-					player_table[ply:SteamID64()] = {0, 0}
-				else
-					error("An error occurred whilst inserting new player")
+		do -- On Connect Sync (Send)
+			function Player_Hook.OnConnect["PlayerModule::Sync::OnConnect"](ply, info)
+				local steamid64_to_ply = {}
+				local player_amount = 0
+
+				for k,v in ipairs(player.GetAll()) do
+					player_amount = k
+					steamid64_to_ply[v:SteamID64()] = v:UserID()
 				end
+
+				J_NET:Start(Sync_OnConnect)
+				net.WriteUInt(player_amount, 8)
+				for k,v in pairs(player_table) do
+					writePlayerInfo(steamid64_to_ply[k], k[1], k[2])
+				end
+				net.Send(ply)
 			end
+		end
 
-			Player_Hook_Run("OnConnect")(ply, player_table[ply:SteamID64()])
-		end)
-
-		hook.Add("PlayerDisconnected", J_NET:GetNetworkString(Update_OnDisconnect), function (ply)
-			PlayerTable:UpdateLastConnected(ply:SteamID64())
-			player_table[ply:SteamID64()][2] = os.time()
-			Player_Hook_Run("OnDisconnect", function () player_table[ply:SteamID64()] = nil end)(ply, player_table[ply:SteamID64()])
-		end)
-
-		function Player_Hook.OnConnect["PlayerModule::Sync::OnConnect"](ply, info)
-			J_NET:Start(Sync_OnConnect)
-			net.WriteUInt(player.GetCount(), 8)
-			for k,v in pairs(player_table) do
-				net.WriteString(k)
-				net.WriteUInt(k[1], 32)
-				net.WriteUInt(k[2], 32)
+		do -- On Connect Update Clients (Send)
+			function Player_Hook.OnConnect["PlayerModule::Update::OnConnect"](ply, info)
+				J_NET:Start(Update_OnConnect)
+				net.WriteString(ply:UserID())
+				net.WriteUInt(info[1], 32)
+				net.WriteUInt(info[2], 32)
+				net.SendOmit(ply)
 			end
-			net.Send(ply)
 		end
 
-		function Player_Hook.OnConnect["PlayerModule::Update::OnConnect"](ply, info)
-			J_NET:Start(Update_OnConnect)
-			net.WriteString(ply:SteamID64())
-			net.WriteUInt(info[1], 32)
-			net.WriteUInt(info[2], 32)
-			net.SendOmit(ply)
+		do -- On Disconnect Remove from Table (Send) && On Last Connected Update (Send)
+			function Player_Hook.OnDisconnect["PlayerModule::Update::OnDisconnect"](ply, info)
+				J_NET:Start(Update_OnDisconnect)
+				writePlayerInfo(ply, info[1], info[2])
+				net.Broadcast()
+			end
 		end
 
-		function Player_Hook.OnDisconnect["PlayerModule::Update::OnDisconnect"](ply, info)
-			J_NET:Start(Update_OnDisconnect)
-			net.WriteString(ply:SteamID64())
-			net.Broadcast()
+		do -- On Code Update (Send)
+			function Player_Hook.OnModifiedCode["PlayerModule::CodeModified"](ply, new_value, old_value)
+				J_NET:Start(Update_PlayerCode)
+				net.WriteString(ply:UserID())
+				net.WriteUInt(new_value, 32)
+				net.Broadcast()
+			end
 		end
 
-		function MODULE.Server:Post()
-			local PermissionModule = JAAS:GetModule("Permission")
+		do -- Code Modify (Receive)
+			function MODULE.Server:Post()
+				local PermissionModule = JAAS:GetModule("Permission")
 
-			local CanModifyCode = PermissionModule:RegisterPermission("Can Modify Player Code")
+				local CanModifyCode = PermissionModule:RegisterPermission("Can Modify Player Code")
 
-			J_NET:Receive(Update_Modify, function (len, ply)
-				local msg = ModificationMessage():NetRead()
+				J_NET:Receive(Update_Modify, function (len, ply)
+					local msg = ModificationMessage():NetRead()
 
-				if CanModifyCode:Check(ply:GetCode()) then
-					local target = msg:GetPlayer()
-					if ply:CanTarget(target) then
-						if MODULE:XorPlayerCode(target, msg:GetRank():GetCode()) then
+					if CanModifyCode:Check(ply:GetCode()) then
+						local target = msg:GetPlayer()
+						if ply:CanTarget(target) then
+							if MODULE:XorPlayerCode(target, msg:GetRank():GetCode()) then
+							else
+							end
 						else
 						end
 					else
 					end
-				else
-				end
-			end)
+				end)
+			end
 		end
 	elseif CLIENT then
-		function MODULE:SetPlayerCode(ply, rank_object)
-			ModificationMessage():ModifyPlayerCode(ply, rank_object):SendToServer()
+		do -- Code Modify (Send)
+			function MODULE:SetPlayerCode(ply, rank_object)
+				ModificationMessage():ModifyPlayerCode(ply, rank_object):SendToServer()
+			end
 		end
 
-		local function readPlayerInfo()
-			local ply = net.ReadString()
+		local function readPlayerInfo() -- Player NetRead Function
+			local ply = player.GetByID(net.ReadUInt(32))
 			local code = net.ReadUInt(32)
 			local last = net.ReadUInt(32)
 
-			player_table[ply] = {code, last}
+			player_table[ply:SteamID64()] = {code, last}
+			return ply
 		end
 
-		J_NET:Receive(Sync_OnConnect, function ()
-			local player_amount = net.ReadUInt(8)
-			local index = 1
-			repeat
-				readPlayerInfo()
+		do -- On Connect Sync (Receive)
+			J_NET:Receive(Sync_OnConnect, function ()
+				local player_amount = net.ReadUInt(8)
+				local index = 1
+				repeat
+					readPlayerInfo()
 
-				index = 1 + index
-			until (index <= player_amount)
-		end)
+					index = 1 + index
+				until (index <= player_amount)
+			end)
+		end
 
-		J_NET:Receive(Update_OnConnect, function ()
-			readPlayerInfo()
-		end)
+		do -- On Connect Update Clients (Receive) + Hook
+			J_NET:Receive(Update_OnConnect, function ()
+				local ply = readPlayerInfo()
+				Player_Hook_Run("OnConnect")(ply, player_table[ply:SteamID64()])
+			end)
+		end
 
-		J_NET:Receive(Update_OnDisconnect, function ()
-			player_table[net.ReadString()] = nil
-		end)
+		do -- On Disconnect Remove from Table (Receive) + Hook && On Last Connected Update (Receive)
+			J_NET:Receive(Update_OnDisconnect, function ()
+				local ply = readPlayerInfo()
+				Player_Hook_Run("OnDisconnect", function () player_table[ply:SteamID64()] = nil end)(ply, player_table[ply:SteamID64()])
+			end)
+		end
+
+		do -- On Code Update (Receive) + Hook
+			J_NET:Receive(Update_PlayerCode, function ()
+				local ply = player.GetByID(net.ReadUInt(32))
+				local code = net.ReadUInt(32)
+
+				player_table[ply:SteamID64()][1] = code
+				Player_Hook_Run("OnModifiedCode")(ply, code)
+			end)
+		end
 	end
 end
